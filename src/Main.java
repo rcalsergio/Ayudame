48caed2e73d80d084d873763b39ff3966ca6284e
Santiago Pérez Carral <a25santiagopc@teis.intranet>
2025-09-29 17:22:46 +0200

    Tarea tests validada

--- src/Main.java
+++ src/Main.java
@@ -15,24 +15,54 @@ public class Main {
 
     // 2. Calcula el empuje de un motor (F = m_dot * v_e)
     public static double calcularEmpuje(double flujoMasicoKgPorS, double velocidadEscapeMPorS) {
-        return 0;
+      double f = flujoMasicoKgPorS * velocidadEscapeMPorS;
+      return f;
     }
-
     // 3. Convierte nombre de planeta en su número de órbita desde el Sol
     public static int numeroOrbita(String planeta) {
-       return 0;
+      int numOrbita = 0;
+      switch (planeta.toLowerCase()) { // Convertimos a minúsculas para evitar problemas con mayúsculas
+        case "mercurio":
+          return numOrbita=1;
+        case "venus":
+          return numOrbita=2;
+        case "tierra":
+          return numOrbita=3;
+        case "marte":
+          return numOrbita=4;
+        case "jupiter":
+          return numOrbita=5;
+        case "saturno":
+          return numOrbita=6;
+        case "urano":
+          return numOrbita=7;
+        case "neptuno":
+          return numOrbita=8;
+        default:
+          return numOrbita=-1 ; // Para planetas no válidos o desconocidos
+      }
 
     }
 
     // 4. Promedio de altitudes de satélites
     public static double promedioAltitudes(double[] altitudesKm) {
-       return 0;
+      if (altitudesKm == null || altitudesKm.length == 0) {
+        return 0; // O lanzar una excepción, dependiendo del caso
+      }
+      double suma = 0;
+      for (int i = 0; i < altitudesKm.length; i++) {
+        suma += altitudesKm[i];
+      }
+      double promedio = suma / altitudesKm.length;
+      return promedio;
     }
+
 
     // 5. Convierte segundos a formato horas:minutos:segundos (útil para misiones espaciales)
     public static String convertirTiempo(int segundos) {
-
-        //return String.format("%02d:%02d:%02d", h, m, s);
-        return "";
+      int h= segundos / 3600;
+      int m = (segundos % 3600) / 60;
+      int s = (segundos % 3600) % 60;
+      return String.format("%02d:%02d:%02d", h, m, s);
     }
 }
